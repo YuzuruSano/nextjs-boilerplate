8a552985cbcfc1390ba3ed504864e253e97ab768
YuzuruSano <ysano@oikaze.jp>
2021-04-04 01:50:36 +0900

    add extend css sample

---
+++ src/pages/components/Btn.tsx
@@ -0,0 +1,15 @@
+import tw, { css } from 'twin.macro'
+
+export const btn = css`
+  padding:10px 20px;
+  text-decoration: none;
+  &:hover {
+    transition: all .3s;
+    opacity: 0.6;
+  }
+`
+
+export const btnRed = css`
+  ${btn}
+  border:1px solid red;
+`
